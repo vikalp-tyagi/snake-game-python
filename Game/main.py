91af9e1637ba599a6d52e1abde43f8081b6cba39
Vikalp <vickyt.1309@gmail.com>
2025-01-13 20:08:18 +0530

    Update main.py

--- Game/main.py
+++ Game/main.py
@@ -1,3 +1,23 @@
+'''
+snake-game-python
+Copyright (C) 2024 vikalp-tyagi
+
+For more information or to contact the author, please reach me at:
+Email: vickyt.1309@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+<https://www.gnu.org/licenses/>
+'''
+
 import turtle
 import random
 import time
